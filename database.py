8f188cd717d97fd2c8b4fca7eb783d9d37009806
seohannyeong <shn7601@gmail.com>
2025-11-27 13:24:49 +0900

    Feat: 회원가입 app.py, database.py추가

--- database.py
+++ database.py
@@ -22,3 +22,21 @@ def get_db_conn():
     except oracledb.Error as e:
         print(f"DB 연결 실패: {e}")
         return None
+    
+def register_user(user_id, password, nickname):
+    conn = get_db_conn()
+    if not conn: return False
+
+    cursor = conn.cursor()
+    try:
+        sql = "INSERT INTO USER_T (user_id, password, nickname) VALUES (:1, :2, :3)"
+        cursor.execute(sql, (user_id, password, nickname))
+        conn.commit()
+        return True
+    except oracledb.Error as e:
+        print(f"회원가입 실패: {e}")
+        conn.rollback()
+        return False
+    finally:
+        if cursor: cursor.close()
+        if conn: conn.close()
